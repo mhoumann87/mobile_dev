274e4c4e78da1d0e621d306bcafa89dfba9f7a8a
Michael Houmann <michael@michael-h.dk>
2017-02-06 20:57:44 +0100

    Mandag uge 6

---
+++ skateShop/src/models/board.ts
@@ -0,0 +1,15 @@
+export class Board {
+
+    brand:  string;
+    model: string;
+    color: string;
+    price: number;
+
+    constructor(brand: string, model: string, color: string, price: number ) {
+
+        this.brand = brand;
+        this,model = model;
+        this.color = color;
+        this.price = price;
+    }
+}
