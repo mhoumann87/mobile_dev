e27b15963626a8b88207ad75e61b315258dc9617
Michael Houmann <michael@michael-h.dk>
2017-02-07 23:37:27 +0100

    Skateshop basis done

--- skateShop/src/models/board.ts
+++ skateShop/src/models/board.ts
@@ -1,15 +1,16 @@
-export class Board {
+export class Board
+{
+    brand:string;
+    model:string;
+    color:string;
+    price:number;
 
-    brand:  string;
-    model: string;
-    color: string;
-    price: number;
-
-    constructor(brand: string, model: string, color: string, price: number ) {
-
+    constructor(brand:string, model:string, color:string, price:number )
+    {
         this.brand = brand;
-        this,model = model;
+        this.model = model;
         this.color = color;
         this.price = price;
     }
+    
 }
--- skateShop/src/pages/home/home.ts
+++ skateShop/src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
+
 import { NavController, AlertController } from 'ionic-angular';
-import { Board} '../../models/board';
+import { Board } from '../../models/board';
 
 @Component({
   selector: 'page-home',
@@ -8,8 +9,38 @@ import { Board} '../../models/board';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private alertCtrl: AlertController) {
+    this.boards.push(new Board("myModel1", "myBrand1", "Blue", 399 ));
+    this.boards.push(new Board("myModel2", "myBrand2", "Red", 399 ));
+    this.boards.push(new Board("myModel3", "myBrand3", "Black", 399 ));
     
+    this.selectedBoard = new Board("","","",0);
   }
+  public selectedBoard:Board;
 
+  public boards:Array<Board> = new Array<Board>();
+
+  buySkateboard()
+  {
+      let alert = this.alertCtrl.create({
+    title: 'Confirm purchase',
+      message: 'Do you want to buy this skateboard: ' + JSON.stringify(this.selectedBoard),
+    buttons: [
+      {
+        text: 'Cancel',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      },
+      {
+        text: 'Buy',
+        handler: () => {
+          console.log('Buy clicked');
+        }
+      }
+    ]
+  });
+  alert.present();
+  }
 }
