612fa04167ecfe190074dd742c6a174ded571a57
Michael Houmann <michael@michael-h.dk>
2017-02-21 10:34:46 +0100

    skateshop new page

--- skateShop/src/app/app.component.ts
+++ skateShop/src/app/app.component.ts
@@ -3,13 +3,13 @@ import { Platform } from 'ionic-angular';
 import { StatusBar, Splashscreen } from 'ionic-native';
 
 import { HomePage } from '../pages/home/home';
-
+import { WelcomePage } from '../pages/welcome/welcome';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = HomePage;
+  rootPage = WelcomePage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
