23e77aed9139513c0a15f5c9138e824be79ec799
Michael Houmann <michael@michael-h.dk>
2017-03-15 13:54:20 +0100

    15-03-2017

--- TodoApp/src/app/app.module.ts
+++ TodoApp/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import { SparetimePage } from '../pages/sparetime/sparetime';
+import { IonicStorageModule } from '@ionic/storage';
 
 
 @NgModule({
@@ -18,7 +19,9 @@ import { SparetimePage } from '../pages/sparetime/sparetime';
     SparetimePage
   ],
   imports: [
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- TodoApp/src/pages/home/home.ts
+++ TodoApp/src/pages/home/home.ts
@@ -1,14 +1,23 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
-
+import { Storage } from '@ionic/storage';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, storage: Storage) {
+
+    storage.ready().then(()=>{
+
+      storage.set('name', 'Michael');
+
+      storage.get('name').then((val) => {
+        console.log('Your name is', val);
+      })
+    });
 
   }
 
